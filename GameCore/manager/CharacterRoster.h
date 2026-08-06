6e2f5db9c7a4f78c28b106b45c8cb3573eeb0761
Dung Dinh <100460297+dungprolhvn@users.noreply.github.com>
2026-08-06 10:07:56 +0700

    Them 3 method: findByName, updateMage, updateWarrior. Them cac setter can thiet cho Mage va Warrior

--- GameCore/manager/CharacterRoster.h
+++ GameCore/manager/CharacterRoster.h
@@ -3,6 +3,10 @@
 #include <memory>
 #include "../model/Character.h"
 
+/*
+	Author: Nguyen Dinh Dung
+	Date modified: 2026-08-06
+*/
 class CharacterRoster
 {
 private:
@@ -13,6 +17,9 @@ public:
 	int add(std::unique_ptr<Character> character); // thêm 1 character. trả về character id nếu thành công, -1 nếu thất bại (do trùng id, tên không hợp lệ,...)
 	const Character* findById(int charId) const; // Tuyệt đối KHÔNG ĐƯỢC XÓA con trỏ trả về. Tìm nhân vật theo Id, trả về con trỏ tới object nếu thấy, null nếu ko tồn tại.
 	Character* findById(int charId); // tìm nhân vật theo Id (phiên bản có thể update), tuyệt đối KHÔNG ĐƯỢC XÓA con trỏ trả về
+	std::vector<Character*> findByName(const std::string& name) const; // tìm nhân vật theo tên, trả về vector chứa con trỏ tới tất cả nhân vật có tên gần giống với name. Nếu ko có thì trả về vector rỗng. Tuyệt đối KHÔNG ĐƯỢC XÓA con trỏ trả về.
+	bool updateWarrior(int charId, const std::string& newName, unsigned int newMaxHp, int newAttackPower); // cập nhật thông tin nhân vật Warrior theo character.id. trả về true nếu thành công, false nếu thất bại (do ko tìm thấy charId hoặc thông tin mới ko hợp lệ)
+	bool updateMage(int charId, const std::string& newName, unsigned int newMaxHp, unsigned int newMaxMana, unsigned int newSpellDamage, unsigned int newManaCost, unsigned int newFallbackDamage); // cập nhật thông tin nhân vật Mage theo character.id. trả về true nếu thành công, false nếu thất bại (do ko tìm thấy charId hoặc thông tin mới ko hợp lệ)
 	bool remove(int charId); // xóa nhân vật khỏi roster theo character.id
 
 	bool isEmpty() const; // trả về true nếu ko có nhân vật nào.
--- GameCore/model/Character.h
+++ GameCore/model/Character.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 
+const unsigned int CHARACTER_MAX_HP_UPPER = 500;
+const unsigned int CHARACTER_MAX_HP_LOWER = 1;
+
 class Character
 {
 protected:
@@ -20,6 +23,9 @@ public:
 	int getMaxHp() const;
 	std::string getName() const;
 	std::string getType() const;
+	// setter
+	bool setName(const std::string& newName);
+	bool setMaxHp(unsigned int newMaxHp);
 	// display
 	virtual void display() const;
 	// business logic
